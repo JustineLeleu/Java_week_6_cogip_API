25aefd35ce53bd1b5f3dca84afa72c5edab1441a
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-01-24 15:42:27 +0100

    add dto and hash

--- src/main/java/week6/java/cogip/controllers/UserController.java
+++ src/main/java/week6/java/cogip/controllers/UserController.java
@@ -1,24 +1,30 @@
 package week6.java.cogip.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import week6.java.cogip.dtos.UserDto;
 import week6.java.cogip.entities.User;
 import week6.java.cogip.service.UserService;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-
-  @Autowired
-  private UserService userService;
   
-  @GetMapping
+  // final + method above = @Autowired
+  private final UserService userService;
+  
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+  
+  @GetMapping("/all")
   public ResponseEntity<?> getAllUsers() {
     return ResponseEntity.ok(userService.getAllUsers());
   }
@@ -32,28 +38,34 @@ public class UserController {
       }
     }
     catch (ResponseStatusException e) {
-      return new ResponseEntity<>(id + "User not found", HttpStatus.NOT_FOUND);
+      return new ResponseEntity<>(id + " User not found", HttpStatus.NOT_FOUND);
     }
   }
   @PostMapping
-  public ResponseEntity<Object> createUser(@RequestParam String username,
-                                           @RequestParam String password,
+  public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto,
                                            @RequestParam(required = false, defaultValue = "USER") String role) {
-    User user = new User(username, password, role);
-    userService.createUser(user);
-    return ResponseEntity.ok(user);
+    try {
+      User user = userDto.dtoUser(new User());
+      user.setRole(role);
+      userService.createUser(user);
+      return ResponseEntity.ok(user);
+    }
+    catch (ResponseStatusException e) {
+      return new ResponseEntity<>("User can't be created", HttpStatus.NOT_FOUND);
+    }
   }
   @PutMapping("/{id}")
   public ResponseEntity<Object> updateUser(@PathVariable Short id,
                                            @RequestParam(required = false) String username,
                                            @RequestParam(required = false) String password,
                                            @RequestParam(required = false) String role) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     Optional<User> optionalUser = userService.getUser(id);
     try{
     if (optionalUser.isPresent()) {
       User user = optionalUser.get();
       if (username != null) user.setUsername(username);
-      if (password != null) user.setPassword(password);
+      if (password != null) user.setPassword(encoder.encode(password));
       if (role != null) user.setRole(role);
       userService.createUser(user);
       return ResponseEntity.ok(user);
