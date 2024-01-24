914300e79b49143acefe31605eedf6e61fe6c768
Virginie Dourson <virginiedourson@gmail.com>
2024-01-24 12:13:45 +0100

    Get companies with related invoices

--- src/main/java/week6/java/cogip/entities/Company.java
+++ src/main/java/week6/java/cogip/entities/Company.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,12 +16,12 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "company")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Company {
 	
 	@Id
@@ -34,6 +38,14 @@ public class Company {
 	@Column(name = "vat")
     private String tva;
 	
+	public List<Invoice> getInvoices() {
+		return invoices;
+	}
+
+	public void setInvoices(List<Invoice> invoices) {
+		this.invoices = invoices;
+	}
+
 	@Column(name= "type")
     private String type;
 	
@@ -44,7 +56,8 @@ public class Company {
 	@OneToMany(
 			   mappedBy = "company",
 			   cascade = CascadeType.ALL, 
-			   orphanRemoval = true 
+			   orphanRemoval = true,
+			   fetch = FetchType.EAGER
 			  )
 	private List<Invoice> invoices = new ArrayList<>();
 
