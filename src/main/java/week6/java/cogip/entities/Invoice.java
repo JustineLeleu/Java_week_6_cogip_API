e1dda05e8c4e1300e7a5ac86717f566ec4cf2133
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-01-26 10:09:56 +0100

    delete getter and setter invocie

--- src/main/java/week6/java/cogip/entities/Invoice.java
+++ src/main/java/week6/java/cogip/entities/Invoice.java
@@ -11,78 +11,50 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Getter
+@Setter
 public class Invoice {
-	
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private short id;
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private short id;
   
   @Column(name = "timestamp", nullable = false, updatable = false, insertable = false)
   private String timestamp;
-    
-    @ManyToOne(
-    		cascade = { 
-					CascadeType.PERSIST, 
-					CascadeType.MERGE 
-					} 
-    		)
-    @JoinColumn(name= "invoice_company_id")
-    private Company company;
-    
-    @ManyToOne(
-            cascade = {
-                    CascadeType.PERSIST,
-                    CascadeType.MERGE
-            }
-    )
-    @JoinColumn(name= "invoice_contact_id")
-    private Contact contact;
-    
-    public Invoice(short id, String timestamp, Company company, Contact contact) {
-        this.id = id;
-        this.timestamp = timestamp;
-        this.company = company;
-        //this.contact = contact;
-    }
-
-	public Invoice() {
-
-	}
-	
-	public short getId() {
-        return id;
-    }
-
-    public void setId(short id) {
-        this.id = id;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Company getCompany() {
-		return company;
-	}
-
-	public void setCompany(Company company) {
-		this.company = company;
-	}
-
-	public Contact getContact() {
-		return contact;
-	}
-
-	public void setContact(Contact contact) {
-		this.contact = contact;
-	}
-
+  
+  @ManyToOne(
+          cascade = {
+                  CascadeType.PERSIST,
+                  CascadeType.MERGE
+          }
+  )
+  @JoinColumn(name = "invoice_company_id")
+  private Company company;
+  
+  @ManyToOne(
+          cascade = {
+                  CascadeType.PERSIST,
+                  CascadeType.MERGE
+          }
+  )
+  @JoinColumn(name = "invoice_contact_id")
+  private Contact contact;
+  
+  public Invoice(short id, String timestamp, Company company, Contact contact) {
+    this.id = id;
+    this.timestamp = timestamp;
+    this.company = company;
+    //this.contact = contact;
+  }
+  
+  public Invoice() {
+  
+  }
+  
 }
