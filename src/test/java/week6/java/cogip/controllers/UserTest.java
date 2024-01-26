17e8dac8af7edb5191a23ae60761e869bd7285cf
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-01-26 13:54:26 +0100

    add UserTests comments

--- src/test/java/week6/java/cogip/controllers/UserTest.java
+++ src/test/java/week6/java/cogip/controllers/UserTest.java
@@ -1,5 +1,6 @@
 package week6.java.cogip.controllers;
 
+// All imports for the class UserTest
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.MethodOrderer;
@@ -20,26 +21,42 @@ import java.util.Map;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
+// @SpringBootTest is used to specify that the context under test is a @SpringBootApplication
+// @AutoConfigureMockMvc is used to configure MockMvc
+// MockMvc is used to perform requests to the application without starting the server
+// @TestMethodOrder is used to specify the order in which the tests are executed
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserTest {
   
+  // @Autowired is used to inject the MockMvc object
+  // MockMvc is used to perform requests to the application without starting the server
+  // id variable is used to store the id of the user created in the PostTest() method
   @Autowired
   private MockMvc mockMvc;
   static private int id;
   
-  
+  // @Test is used to specify that the method is a test
+  // @Order is used to specify the order in which the tests are executed
+  // throws Exception is used to throw an exception if the test fails
+  // MockMvcRequestBuilders is used to perform requests to the application without starting the server
+  // status() is used to check if the status of the response is the same as the expected status
+  // $[0].id is used to get the id of the first user in the response
+  // Get "all" users from the database
   @Test
   @Order(1)
   void GetTest() throws Exception {
-    MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get("/api/user/all"))
+    MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get("/api/user"))
             .andExpect(status().isOk())
             .andReturn();
     String response = result.getResponse().getContentAsString();
     id = JsonPath.parse(response).read("$[0].id");
   }
   
+  // Post a new user in the database
+  // Map is used to store the username and password of the user
+  // ObjectMapper is used to convert the Map to a JSON string
   @Test
   @Order(2)
   void PostTest() throws Exception {
@@ -58,6 +75,7 @@ class UserTest {
             .andExpect(status().isOk());
   }
   
+  // Get a specific user by ID from the database
   @Test
   @Order(3)
   void GetByIdTest() throws Exception {
@@ -67,6 +85,7 @@ class UserTest {
             .andExpect(status().isOk());
   }
   
+  // Put (edit) a user with a specific ID in the database
   @Test
   @Order(4)
   void PutTest() throws Exception {
@@ -84,6 +103,7 @@ class UserTest {
             .andExpect(status().isOk());
   }
   
+  // Delete a user with a specific ID in the database
   @Test
   @Order(5)
   void DeleteTest() throws Exception {
