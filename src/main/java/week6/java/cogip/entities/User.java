9c3772a47c361815a2b194dba5cef4279abd134b
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-01-26 11:25:44 +0100

    add entities comments

--- src/main/java/week6/java/cogip/entities/User.java
+++ src/main/java/week6/java/cogip/entities/User.java
@@ -1,9 +1,16 @@
 package week6.java.cogip.entities;
 
+// All imports for the User entity
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+// The User entity
+// The @Entity annotation tells Spring that this is a JPA entity, and it should be mapped to a database table.
+// The @Table annotation specifies the name of the database table to be used for mapping.
+// The @Id annotation specifies the primary key of an entity.
+// The @GeneratedValue annotation is used to specify the primary key generation strategy to use.
+// The @Setter and @Getter annotations from Lombok are used to generate the setters and getters for the entity.
 @Entity
 @Table(name = "user")
 public class User {
