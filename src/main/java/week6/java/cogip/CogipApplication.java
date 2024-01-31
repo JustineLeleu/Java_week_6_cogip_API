7f7e74da23c16c4bd972c1560cef9a640d980437
Virginie Dourson <virginiedourson@gmail.com>
2024-01-31 14:07:01 +0100

    WIP Command companies: displays names, vats and invoices ids

--- src/main/java/week6/java/cogip/CogipApplication.java
+++ src/main/java/week6/java/cogip/CogipApplication.java
@@ -2,6 +2,7 @@ package week6.java.cogip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CogipApplication {
