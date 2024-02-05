c7b645f7f2f06c44d46d0760ae909ed82a3fa92f
Virginie Dourson <virginiedourson@gmail.com>
2024-02-05 16:41:00 +0100

    Clean and add comments

--- src/main/java/week6/java/cogip/service/CompanyService.java
+++ src/main/java/week6/java/cogip/service/CompanyService.java
@@ -20,30 +20,32 @@ public class CompanyService {
 		this.companyRepository = companyRepository;
 	}
 
+	//Getter
 	public CompanyRepository getCompanyRepository() {
 		return companyRepository;
 	}
 
+	//Setter
 	public void setCompanyRepository(CompanyRepository companyRepository) {
 		this.companyRepository = companyRepository;
 	}
 
+	//Returns all companies
 	public Iterable<Company> getCompanies() {
 		return companyRepository.findAll();
 	}
 	
+	//Returns all companies with given type (client or provider)
 	public Iterable<Company> getCompaniesByType(String type){
 		return companyRepository.findByType(type);
 	}
 
+	//Returns company with given (existing) id
 	public Company getCompany(Short companyId) {
 		return companyRepository.findById(companyId).orElseThrow(() -> new NoSuchElementException("No company with ID " + companyId));
 	}
 	
-	public Optional<Company> getCompanyWithException(Short companyId) {
-		return companyRepository.findById(companyId);
-	}
-
+	//Adds a new company
 	public Company saveCompany(Company company) {
 		try {
 			companyRepository.save(company);
@@ -54,6 +56,7 @@ public class CompanyService {
 		return company;
 	}
 	
+	//Updates a company with given (existing) id
 	public Company updateCompany(Company company, Short id) {
 		getCompany(id);
 		company.setId(id);
@@ -66,7 +69,9 @@ public class CompanyService {
 		return company;
 	}
 
+	//Deletes a company with given (existing) id
 	public void delete(Short companyId) {
+		getCompany(companyId);
 		companyRepository.deleteById(companyId);
 	}
 
